a0ac6a4a406224bef01b8f2b1448d1eb9cce9060
Jianqi Pan <jannchie@gmail.com>
2025-04-23 23:27:45 +0900

    chore(dependencies): modify pyproject.toml && remove rye lock files && add uv.lock for dependencies

--- src/inch/__init__.py
+++ src/inch/__init__.py
@@ -1,40 +1,39 @@
-from concurrent.futures import ThreadPoolExecutor
-from datetime import datetime
-from queue import Queue
 import threading
 from abc import ABC, abstractmethod
-from rich.progress import (
-    Progress,
-    TaskID,
-    BarColumn,
-    TimeRemainingColumn,
-    TextColumn,
-    SpinnerColumn,
-    MofNCompleteColumn,
-    TimeElapsedColumn,
-    TaskProgressColumn,
-)
+from concurrent.futures import ThreadPoolExecutor
+from datetime import UTC, datetime
+from queue import Queue
 
 from rich import get_console
+from rich.progress import (
+    BarColumn,
+    MofNCompleteColumn,
+    Progress,
+    SpinnerColumn,
+    TaskID,
+    TaskProgressColumn,
+    TextColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+)
 
 console = get_console()
 
 
 class Inch(ABC):
-
-    def __init__(self, name: str, total: int | None = None, completed: int = 0):
+    def __init__(self, name: str, total: int | None = None, completed: int = 0) -> None:
         self.name = name
         self.total = total
         self.completed = completed
 
     @abstractmethod
-    def __call__(self):
+    def __call__(self) -> None:
         pass
 
 
 # Inch class handling task execution and progress
 class InchPoolExecutor:
-    def __init__(self, name: str = "Inch", max_workers: int = 8):
+    def __init__(self, name: str = "Inch", max_workers: int = 8) -> None:
         self.__progress: Progress = Progress(
             SpinnerColumn(style="yellow"),
             TextColumn(" {task.description}"),
@@ -54,44 +53,39 @@ class InchPoolExecutor:
         self.max_workers = max_workers
         self.name = name
 
-    def __enter__(self):
-        self.started_at = datetime.now()
+    def __enter__(self) -> "InchPoolExecutor":
+        # Initialize the progress bar
+        self.started_at = datetime.now(UTC)
         console.print(f":rocket: Start running [bold]{self.name}[/bold]...")
         self.__initialize_overall_progress()
-        self.progress_thread = threading.Thread(
-            target=self.__update_task_progress, daemon=True
-        )
+        self.progress_thread = threading.Thread(target=self.__update_task_progress, daemon=True)
         self.__main_thread = threading.Thread(target=self.__run_tasks, daemon=True)
         self.__main_thread.start()
         self.progress_thread.start()
         return self
 
-    def __exit__(self, *_):
+    def __exit__(self, *_: object) -> None:
         self.__finish_event.wait()
         self.__main_thread.join()
         self.progress_thread.join()
-        self.finished_at = datetime.now()
-        console.print(
-            f":white_check_mark: Finished in {self.finished_at - self.started_at}"
-        )
+        self.finished_at = datetime.now(UTC)
+        console.print(f":white_check_mark: Finished in {self.finished_at - self.started_at}")
 
-    def start_inch(self, task: Inch):
+    def start_inch(self, task: Inch) -> None:
         self.__total_task_count += 1
         if overall_task := self.__progress.tasks[self.__overall_task_id]:
             overall_task.total = self.__total_task_count
         self.__pending_tasks.put(task)
 
-    def __initialize_overall_progress(self):
+    def __initialize_overall_progress(self) -> None:
         # Adding the overall progress bar
-        self.__overall_task_id = self.__progress.add_task(
-            self.name, total=self.__total_task_count
-        )
+        self.__overall_task_id = self.__progress.add_task(self.name, total=self.__total_task_count)
 
     def run(self):
         self.__enter__()
         self.__exit__(None, None, None)
 
-    def __update_task_progress(self):
+    def __update_task_progress(self) -> None:
         self.__progress.start()
         while not self.__finish_event.wait(timeout=0.05):
             # 加锁，避免多线程操作 running_tasks 时出现问题
@@ -100,9 +94,8 @@ class InchPoolExecutor:
                     self.__progress.update(task_id, completed=task.completed)
         self.__progress.stop()
 
-    def __run_tasks(self):
-
-        def run_task(task: Inch):
+    def __run_tasks(self) -> None:
+        def run_task(task: Inch) -> None:
             task_id = self.__progress.add_task(task.name, total=task.total)
             with self.__lock:
                 self.__running_tasks[task_id] = task
@@ -115,16 +108,12 @@ class InchPoolExecutor:
             # Update the global progress bar when a task is completed
             with self.__lock:
                 self.__completed_task_count += 1
-                self.__progress.update(
-                    self.__overall_task_id, completed=self.__completed_task_count
-                )
+                self.__progress.update(self.__overall_task_id, completed=self.__completed_task_count)
 
             if task_id in self.__running_tasks:
                 del self.__running_tasks[task_id]
             self.__progress.remove_task(task_id)
-            self.__running_tasks = {
-                k: v for k, v in self.__running_tasks.items() if v.completed != v.total
-            }
+            self.__running_tasks = {k: v for k, v in self.__running_tasks.items() if v.completed != v.total}
             if not self.__running_tasks and self.__pending_tasks.empty():
                 self.__finish_event.set()
                 # Add a None to the queue to stop the worker threads
@@ -136,4 +125,3 @@ class InchPoolExecutor:
                 if not task:
                     break
                 pool.submit(run_task, task)
-
--- src/inch/__main__.py
+++ src/inch/__main__.py
@@ -1,22 +1,18 @@
-
-
-from inch import Inch, InchPoolExecutor
-
 import random
 from time import sleep
 
+from inch import Inch, InchPoolExecutor
+
 if __name__ == "__main__":
 
-
     class TestTask(Inch):
-        def __call__(self):
+        def __call__(self) -> None:
             while self.completed < self.total:
                 self.completed += random.randint(1, 200)
                 sleep(0.1)
 
     class TestTaskNoProgress(Inch):
-
-        def __call__(self):
+        def __call__(self) -> None:
             completed = 0
             while completed < 1200:
                 completed += random.randint(1, 200)
@@ -25,6 +21,6 @@ if __name__ == "__main__":
     with InchPoolExecutor() as executor:
         for i in range(20):
             if i % 5 == 0:
-                executor.start_inch(TestTaskNoProgress(name=f"Task {i+1}"))
+                executor.start_inch(TestTaskNoProgress(name=f"Task {i + 1}"))
             else:
-                executor.start_inch(TestTask(name=f"Task {i+1}", total=1000))
+                executor.start_inch(TestTask(name=f"Task {i + 1}", total=1000))
